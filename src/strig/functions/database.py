5d351fa72ebb35d3a6c41ccece67c764a93732ab
Wibo Kuipers <wibokuip@gmail.com>
2024-10-08 19:01:11 +0200

    Generate multiple in advance for database

--- src/strig/functions/database.py
+++ src/strig/functions/database.py
@@ -1,4 +1,4 @@
-from typing import Literal, overload, Optional, Any
+from typing import Literal, overload, Optional, Tuple
 from .alpha import alpha
 from .alphanumeric import alphanumeric
 from .numeric import numeric
@@ -8,37 +8,42 @@ types = Literal["mysql", "mongo"]
 methods = Literal["alpha", "alphanumeric", "numeric", "var"]
 
 @overload
-def database(type: types, method: Literal["numeric"]) -> int: ...
+def database(type: types, method: Literal["numeric"], prefix: Optional[int] = None) -> int: ...
 
 @overload
-def database(type: types, method: Literal["alpha", "alphanumeric"]) -> str: ...
+def database(type: types, method: Literal["alpha", "alphanumeric"], prefix: Optional[str] = None) -> str: ...
 
 @overload
-def database(type: types, method: Literal["var"], varstring: str) -> str: ...
+def database(type: types, method: Literal["var"], varstring: str, prefix: Optional[str] = None) -> str: ...
 
-def database(type: types, method: methods, varstring: str = "") -> str | int:
+def database(type: types, method: methods, varstring: str = "", prefix: Optional[str | int] = None) -> str | int:
     if type == "mysql":
         try: 
             import mysql.connector
         except: 
             raise ImportError("The Strig database function requires you to have mysql-connector-python installed. pip install mysql-connector-python")
 
-    def generate():
+    ## Generating multiple strings in advance will be less of a performance cost
+    ## than doing multiple database requests.
+    def generate() -> Tuple[str, str, str, str] | Tuple[int, int, int, int]:
         if method == "alpha":
-            return alpha()
+            return (alpha(), alpha(), alpha(), alpha())
 
         if method == "alphanumeric":
-            return alphanumeric()
+            return (alphanumeric(), alphanumeric(), alphanumeric(), alphanumeric())
 
         if method == "numeric":
-            return numeric()
+            return (numeric(), numeric(), numeric(), numeric())
 
         if method == "var":
-            return var(varstring)
+            return (var(varstring), var(varstring), var(varstring), var(varstring))
 
     generated = generate()
-        
+
+    if type == "mysql":
+        query = f"SELECT " 
+         
 
 
-    return generated
+    return generated[0]
 
